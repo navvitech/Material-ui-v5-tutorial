9c4b9329519a16f77124e521c10c323d9567af7c
navaneeth reddy <navaneethreddyboda@gmail.com>
2021-10-23 20:15:17 +0530

    Icons and IconButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
-import Button from "@mui/material/Button";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import IconButton from "@mui/material/IconButton";
+
+//? 1)predefined
+//? 2)style
+//? 3)sx
+
+//! IconButton
 
 function App() {
   return (
     <div className="App">
       <h1>Material UI</h1>
-      <Button>Login</Button>
+      <IconButton sx={{ color: "red" }}>
+        <FacebookIcon color="warning" fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <WhatsAppIcon style={{ color: "green", fontSize: 70 }} />
+      </IconButton>
+      <IconButton>
+        <YouTubeIcon sx={{ color: "red", fontSize: 70 }} />
+      </IconButton>
     </div>
   );
 }

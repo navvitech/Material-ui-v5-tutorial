44990a42524772b48710a0faef3618b8706bded3
Navaneeth <navaneethreddyboda@gmail.com>
2023-01-15 13:35:55 +0530

    Button Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import "./App.css";
-import Button from "@mui/material/Button";
+import './App.css';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import { Button } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Material UI</h1>
-      <Button>Login</Button>
+      <Button disableElevation variant='text' color='error'>
+        react
+      </Button>
+      <Button disableElevation variant='contained' color='warning'>
+        react
+      </Button>
+      <Button disableElevation variant='contained' color='success'>
+        react
+      </Button>
+      <Button
+        variant='contained'
+        sx={{
+          background: 'blue',
+          ':hover': {
+            background: 'red',
+          },
+          textTransform: 'capitalize',
+          fontWeight: '700',
+        }}
+        disableElevation
+        // disableRipple
+        startIcon={<FacebookIcon />}
+        // endIcon={<FacebookIcon />}
+        size='large'
+        disabled
+      >
+        facebook
+      </Button>
     </div>
   );
 }
